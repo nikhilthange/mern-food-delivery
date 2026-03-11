85e1c3309c4eee72e2813c6a1611c3cc849790a5
Nikhil Thange <nikhilthange075@gmail.com>
2026-03-11 23:31:13 +0530

    Added new API route

--- admin/vite-project/src/main.jsx
+++ admin/vite-project/src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom"
+import './index.css'
+import { BrowserRouter } from "react-router-dom"
 
-createRoot(document.getElementById('root')).render(
- <BrowserRouter><App /></BrowserRouter>
-    
-  
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
